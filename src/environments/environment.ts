423b7096b4570ebf62d615e0546006d4abe1e124
Juan Marcos <juanmarcoscabezas@gmail.com>
2021-01-29 11:19:29 -0500

    Production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyBCv7Pr5RPS6sH1pLzKahDl-XJxnMRLkEE',
     authDomain: 'tulapp-87336.firebaseapp.com',
